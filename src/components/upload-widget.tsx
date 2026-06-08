6817befb2abedcc1217cc4a8e6518f8dfdc66b27
Willian Alves Moreno <willianam@uninove.br>
2026-06-08 20:15:27 -0300

    style: Adicionada largura mínima widget quando aberto

--- src/components/upload-widget.tsx
+++ src/components/upload-widget.tsx
@@ -18,7 +18,7 @@ export function UploadWidget() {
     >
       <motion.div
         animate={isWidgetOpen ? 'open' : 'closed'}
-        className="max-w-90 animate-border-angle overflow-hidden rounded-xl border border-transparent bg-zinc-900 data-[state=closed]:data-[progress=false]:shadow-shape data-[state=closed]:rounded-3xl data-[state=open]:shadow-shape data-[state=closed]:data-[progress=true]:[background:linear-gradient(45deg,#09090B,--theme(--color-zinc-900)_50%,#09090B)_padding-box,conic-gradient(from_var(--border-angle),--theme(--color-zinc-700/.48)_80%,--theme(--color-indigo-500)_86%,--theme(--color-indigo-300)_90%,--theme(--color-indigo-500)_94%,--theme(--color-zinc-600/.48))_border-box]"
+        className="max-w-90 animate-border-angle overflow-hidden rounded-xl border border-transparent bg-zinc-900 data-[state=closed]:data-[progress=false]:shadow-shape data-[state=open]:min-w-72 data-[state=closed]:rounded-3xl data-[state=open]:shadow-shape data-[state=closed]:data-[progress=true]:[background:linear-gradient(45deg,#09090B,--theme(--color-zinc-900)_50%,#09090B)_padding-box,conic-gradient(from_var(--border-angle),--theme(--color-zinc-700/.48)_80%,--theme(--color-indigo-500)_86%,--theme(--color-indigo-300)_90%,--theme(--color-indigo-500)_94%,--theme(--color-zinc-600/.48))_border-box]"
         data-progress={isThereAnyPendingUpload}
         variants={{
           closed: {
