897c8389f2263297334113ec30e2e12d3c0be33a
Willian Alves Moreno <willianam@uninove.br>
2026-06-08 19:46:53 -0300

    style: Borda gradiente no botão

--- src/components/upload-widget.tsx
+++ src/components/upload-widget.tsx
@@ -6,6 +6,8 @@ import { UploadWidgetMinimizedButton } from './upload-widget-minimized-button'
 import { UploadWidgetUploadList } from './upload-widget-upload-list'
 
 export function UploadWidget() {
+  const isThereAnyPendingUpload = true
+
   const [isWidgetOpen, toggleWidgetOpen] = useCycle(false, true)
 
   return (
@@ -16,10 +18,12 @@ export function UploadWidget() {
     >
       <motion.div
         animate={isWidgetOpen ? 'open' : 'closed'}
-        className="w-full max-w-90 overflow-hidden rounded-xl bg-zinc-900 shadow-shape"
+        className="max-w-90 animate-border-angle overflow-hidden rounded-xl border border-transparent bg-zinc-900 data-[state=closed]:data-[progress=false]:shadow-shape data-[state=closed]:rounded-3xl data-[state=open]:shadow-shape data-[state=closed]:data-[progress=true]:[background:linear-gradient(45deg,#09090B,--theme(--color-zinc-900)_50%,#09090B)_padding-box,conic-gradient(from_var(--border-angle),--theme(--color-zinc-700/.48)_80%,--theme(--color-indigo-500)_86%,--theme(--color-indigo-300)_90%,--theme(--color-indigo-500)_94%,--theme(--color-zinc-600/.48))_border-box]"
+        data-progress={isThereAnyPendingUpload}
         variants={{
           closed: {
             height: 44,
+            maxWidth: 'max-content',
             transition: {
               type: 'inertia',
             },
