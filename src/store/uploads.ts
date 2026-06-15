4f5f9525f2770d7a6ed574535f18839e4d7bd477
Willian Alves Moreno <willianam@uninove.br>
2026-06-15 18:53:53 -0300

    feat: Upload dos arquivos

--- src/store/uploads.ts
+++ src/store/uploads.ts
@@ -1,6 +1,7 @@
 import { enableMapSet } from 'immer'
 import { create } from 'zustand'
 import { immer } from 'zustand/middleware/immer'
+import { uploadFileToStorage } from '../http/upload-file-to-storage'
 
 export type Upload = {
   name: string
@@ -15,7 +16,19 @@ type UploadsState = {
 enableMapSet()
 
 export const useUploads = create<UploadsState, [['zustand/immer', never]]>(
-  immer((set) => {
+  immer((set, get) => {
+    async function processUpload(uploadId: string) {
+      const upload = get().uploads.get(uploadId)
+
+      if (!upload) {
+        return
+      }
+
+      await uploadFileToStorage({
+        file: upload.file,
+      })
+    }
+
     function addUploads(files: File[]) {
       for (const file of files) {
         const uploadId = crypto.randomUUID()
@@ -27,6 +40,8 @@ export const useUploads = create<UploadsState, [['zustand/immer', never]]>(
         set((state) => {
           state.uploads.set(uploadId, upload)
         })
+
+        processUpload(uploadId)
       }
 
       return files
