066642ed8de3ed74b7509dab46fb1ecfd1006bd7
Willian Alves Moreno <willianam@uninove.br>
2026-07-11 15:46:51 -0300

    feat: Compressão de imagens com canvas

--- src/store/uploads.ts
+++ src/store/uploads.ts
@@ -4,6 +4,7 @@ import { create } from 'zustand'
 import { immer } from 'zustand/middleware/immer'
 import { useShallow } from 'zustand/shallow'
 import { uploadFileToStorage } from '../http/upload-file-to-storage'
+import { compressImage } from '../utils/compress-image'
 
 export type Upload = {
   name: string
@@ -47,9 +48,16 @@ export const useUploads = create<UploadsState, [['zustand/immer', never]]>(
       }
 
       try {
+        const compressedFile = await compressImage({
+          file: upload.file,
+          maxWidth: 200,
+          maxHeight: 200,
+          quality: 0.5,
+        })
+
         await uploadFileToStorage(
           {
-            file: upload.file,
+            file: compressedFile,
             onProgress(sizeInBytes) {
               updateUpload(uploadId, { uploadSizeInBytes: sizeInBytes })
             },
---
+++ src/utils/compress-image.ts
@@ -0,0 +1,84 @@
+interface CompressImageParams {
+  file: File
+  maxWidth?: number
+  maxHeight?: number
+  quality?: number
+}
+
+function convertToWebp(filename: string): string {
+  return filename.replace(/^(.*?)\.\w+$/, '$1.webp')
+}
+
+export function compressImage({
+  file,
+  maxWidth = Number.POSITIVE_INFINITY,
+  maxHeight = Number.POSITIVE_INFINITY,
+  quality = 1,
+}: CompressImageParams) {
+  const allowedFileTypes = ['image/jpg', 'image/jpeg', 'image/png', 'image/webp']
+
+  if (!allowedFileTypes.includes(file.type)) {
+    throw new Error('Image format not supported')
+  }
+
+  return new Promise<File>((resolve, reject) => {
+    const reader = new FileReader()
+
+    reader.onload = (event) => {
+      const compressed = new Image()
+
+      compressed.onload = () => {
+        const canvas = document.createElement('canvas')
+
+        let width = compressed.width
+        let height = compressed.height
+
+        if (width > height) {
+          if (width > maxWidth) {
+            width = maxWidth
+            height *= maxWidth / width
+          }
+        } else {
+          if (height > maxHeight) {
+            height = maxHeight
+            width *= maxHeight / height
+          }
+        }
+
+        canvas.width = width
+        canvas.height = height
+
+        const context = canvas.getContext('2d')
+
+        if (!context) {
+          reject(new Error('Failed to get canvas context'))
+          return
+        }
+
+        context.drawImage(compressed, 0, 0, width, height)
+
+        canvas.toBlob(
+          (blob) => {
+            if (!blob) {
+              reject(new Error('Failed to compress image.'))
+              return
+            }
+
+            const compressedFile = new File([blob], convertToWebp(file.name), {
+              type: 'image/webp',
+              lastModified: Date.now(),
+            })
+
+            resolve(compressedFile)
+          },
+          'image/webp',
+          quality,
+        )
+      }
+
+      compressed.src = event.target?.result as string
+    }
+
+    reader.readAsDataURL(file)
+  })
+}
